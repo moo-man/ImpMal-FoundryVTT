6da4ae475463af96a1c2f4ef3d8cd7b1c4ea9c89
Russell <russellthurman94@gmail.com>
2022-11-21 20:27:46 -0600

    add linting

--- src/impmal.js
+++ src/impmal.js
@@ -1,19 +1,16 @@
 Hooks.once("init", () => {
 
-
-    // #if _ENV == "development"
-    CONFIG.debug.impmal = true;
-    // #endif
+  // #if _ENV == "development"
+  CONFIG.debug.impmal = true;
+  // #endif
 
   CONFIG.Actor.documentClass = undefined;
   CONFIG.Item.documentClass = undefined;
   CONFIG.ActiveEffect.documentClass = undefined;
   CONFIG.ActiveEffect.sheetClass = undefined;
-  DocumentSheetConfig.registerSheet(JournalEntryPage, "impmal", Level4TextPageSheet, { makeDefault: true, label : "Imperium Maledictum Journal Sheet" });
+  DocumentSheetConfig.registerSheet(JournalEntryPage, "impmal", Level4TextPageSheet, { makeDefault: true, label: "Imperium Maledictum Journal Sheet" });
 
   game.impmal = {
-
+    
   };
-
-
 });
