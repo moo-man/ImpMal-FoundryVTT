7d9cc5e197282ebe2a2106b0681cb91aab3d9185
Russell <russellthurman94@gmail.com>
2022-12-06 23:35:10 -0600

    Add/remove factions

--- src/model/shared/influence.js
+++ src/model/shared/influence.js
@@ -9,5 +9,21 @@ export class InfluenceModel extends foundry.abstract.DataModel
         return schema;
     }
 
-    
+    createFaction(name) 
+    {
+        let factions = duplicate(this.factions);
+
+        // Only add if doesn't exist
+        if (!factions[name])
+        {
+            factions[name.slugify()] = {name, value : 0, notes : ""};
+        }
+        
+        return {factions};
+    }  
+
+    deleteFaction(name) 
+    {
+        return {[`system.influence.factions.-=${name}`] : null};
+    }  
 }
--- src/sheet/actors/actor-sheet.js
+++ src/sheet/actors/actor-sheet.js
@@ -56,6 +56,160 @@ export default class ImpMalActorSheet extends ActorSheet
     {
         super.activateListeners(html);
 
-        html.find("");
+        html.find(".list-edit").on("click", this._onListEdit.bind(this));
+        html.find(".list-delete").on("click", this._onListDelete.bind(this));
+        html.find(".list-create").on("click", this._onListCreate.bind(this));
+        html.find(".list-post").on("click", this._onPostItem.bind(this));
+        html.find(".faction-delete").on("click", this._onFactionDelete.bind(this));
+        html.find(".faction-create").on("click", this._onFactionCreate.bind(this));
+    }
+
+    _onListEdit(event) 
+    {
+        let el = $(event.currentTarget).parents(".list-item");
+        let id = el.attr("data-id");
+        let collection = el.attr("data-collection");
+
+        return this.actor[collection].get[id]?.sheet.render(true);
+    }
+    _onListDelete(event) 
+    {
+        let el = $(event.currentTarget).parents(".list-item");
+        let id = el.attr("data-id");
+        let collection = el.attr("data-collection");
+    
+        let docName = collection == "item" ? "Item" : "ActiveEffect";
+
+        Dialog.confirm({
+            title: game.i18n.localize(`IMPMAL.Delete${docName}`),
+            content: `<p>${game.i18n.localize(`IMPMAL.Delete${docName}Confirmation`)}</p>`,
+            yes: () => {this.actor.deleteEmbeddedDocuments(docName, [id]);},
+            no: () => {},
+            defaultYes: true
+        });
+    }
+    _onListCreate(event) 
+    {
+        let el = $(event.currentTarget).parents(".list-item");
+        let collection = el.attr("data-collection");
+        let docName = collection == "item" ? "Item" : "ActiveEffect";
+        let createData = {};
+
+        if (docName == "Item")
+        {
+            let type = event.currentTarget.dataset.type;
+            createData = { name: `New ${getGame().i18n.localize(CONFIG.Item.typeLabels[type])}`, type };
+        }
+        else 
+        {
+            createData = { name: `New Active Effect`};
+        }
+    
+        return this.actor.createEmbeddedDocuments(docName, [createData]);
+    }
+    
+    _onPostItem(event) 
+    {
+        let itemId = $(event.currentTarget).parents(".item").attr("data-item-id");
+        if (itemId) {return this.actor.items.get(itemId)?.postToChat();}
+    }
+    
+    async _onEffectCreate(ev) 
+    {
+        let type = (ev.currentTarget).dataset["type"];
+        let effectData = { label: getGame().i18n.localize("IMPMAL.NewEffect"), icon: "icons/svg/aura.svg" };
+        if (type == "temporary") 
+        {
+            effectData["duration.rounds"] = 1;
+        }
+    
+        let html = await renderTemplate("systems/IMPMAL-of-eternity/templates/apps/quick-effect.html", effectData);
+        new Dialog({
+            title: getGame().i18n.localize("IMPMAL.QuickEffect"),
+            content: html,
+            buttons: {
+                create: {
+                    label: "Create",
+                    callback: (html) => 
+                    {
+                        html = $(html);
+                        let mode = 2;
+                        let label = html.find(".label").val();
+                        let key = html.find(".key").val();
+                        let value = parseInt(html.find(".modifier").val()?.toString() || "");
+                        effectData.label = label;
+                        effectData.changes = [{ key, mode, value }];
+                        this.actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+                    },
+                },
+                skip: {
+                    label: "Skip",
+                    callback: () => this.actor.createEmbeddedDocuments("ActiveEffect", [effectData]),
+                },
+            },
+            render: (dlg) => 
+            {
+                $(dlg).find(".label").select();
+            },
+        });
+    }
+    
+    _onEffectEdit(ev) 
+    {
+        let id = $(ev.currentTarget).parents(".item").attr("data-item-id");
+        this.object.effects.get(id)?.sheet?.render(true);
+    }
+    
+    _onEffectDelete(ev) 
+    {
+        let id = $(ev.currentTarget).parents(".item").attr("data-item-id");
+        if (id) {this.object.deleteEmbeddedDocuments("ActiveEffect", [id]);}
+    }
+    
+    _onEffectToggle(ev) 
+    {
+        let id = $(ev.currentTarget).parents(".item").attr("data-item-id");
+        let effect = this.object.effects.get(id);
+    
+        if (effect) {effect.update({ disabled: !effect.data.disabled });}
+    }
+
+    _onFactionDelete(ev) 
+    {
+        let el = $(ev.currentTarget).parents(".list-item");
+        let faction = el.attr("data-type");
+        
+        Dialog.confirm({
+            title: game.i18n.localize(`IMPMAL.DeleteFaction`),
+            content: `<p>${game.i18n.localize(`IMPMAL.DeleteFactionConfirmation`)}</p>`,
+            yes: () => {this.actor.update(this.actor.system.influence.deleteFaction(faction));},
+            no: () => {},
+            defaultYes: true
+        });
+    }
+
+    _onFactionCreate() 
+    {
+        new Dialog({
+            title : "IMPMAL.AddInfluence",
+            content : `
+            <form>
+                <div class="form-group">
+                    <label>${game.i18n.localize("IMPMAL.Faction")}</label>
+                    <input type="text">
+                </div>
+            </form>`,
+            buttons : {
+                submit : {
+                    label : game.i18n.localize("Submit"),
+                    callback: (dlg) => 
+                    {
+                        let faction = dlg.find("input")[0].value;
+                        this.actor.update({"system.influence" : this.actor.system.influence.createFaction(faction)});
+                    }
+                }
+            },
+            default : "submit"
+        }).render(true);
     }
 }
