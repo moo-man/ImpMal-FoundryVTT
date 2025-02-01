2f1a584f81be44e1fde1467e4daa4414dfbd86ef
Russell <russellthurman94@gmail.com>
2025-02-01 14:54:19 -0600

    fix augmetic arm script

--- scripts/JjyiHkKRDBb6RYlq.js
+++ scripts/JjyiHkKRDBb6RYlq.js
@@ -1,6 +1,6 @@
         let location = this.effect.getFlag("impmal", "location");
         if (location)
         {
-            args.system.combat.hitLocations[location].armour++;
-            args.system.combat.hitLocations[location].sources.push({name : this.effect.name, value : 1}) 
+            args.actor.system.combat.hitLocations[location].armour++;
+            args.actor.system.combat.hitLocations[location].sources.push({name : this.effect.name, value : 1}) 
         }
