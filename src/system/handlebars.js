d6eb00ec4413d320d348f22fe396617f492ebb84
Russell <russellthurman94@gmail.com>
2025-02-03 18:38:27 -0600

    use library sheetButtons partial

--- src/system/handlebars.js
+++ src/system/handlebars.js
@@ -46,8 +46,7 @@ export default function registerHandlebars()
         "systems/impmal/templates/actor/partials/defendingAgainst.hbs",
         "systems/impmal/templates/actor/partials/influence.hbs",
         "systems/impmal/templates/actor/partials/actions.hbs",
-        "systems/impmal/templates/item/partials/influence.hbs",
-        "systems/impmal/templates/partials/manual-scripts.hbs",
+        "systems/impmal/templates/item/partials/influence.hbs"
     ]);
 
     loadTemplates({
