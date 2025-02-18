0f1c932f6f64aaaf8ef69cb34f16e56fcb660774
Michał Szura <michal.szura@gmail.com>
2025-02-18 23:32:01 +0100

    refactor: Refactor advancement with single check

--- src/apps/advancement.js
+++ src/apps/advancement.js
@@ -19,6 +19,7 @@ export class AdvancementForm extends FormApplication
     {
         super(...args);
         this.actor = this.object.clone();
+        this.canEditXP = game.user.isGM || game.settings.get("impmal", "playerExperienceEditing");
     }
 
     async getData()
@@ -26,6 +27,7 @@ export class AdvancementForm extends FormApplication
         let data = await super.getData();
         this.actor.reset();
         data.actor = this.actor;
+        data.canEditXP = this.canEditXP;
         data.talentsAndPowers = this.actor.itemTypes.power.concat(this.actor.itemTypes.talent);
         return data;
     }
